4377380f823e74b89a85a176a017f8f9297b7792
jzonthemtn <jeff.zemerick@mtnfog.com>
2023-03-18 08:38:44 -0400

    Adding script, model, and ddependencies.

---
+++ convert.py
@@ -0,0 +1,15 @@
+from optimum.onnxruntime import ORTModelForFeatureExtraction
+from transformers import AutoTokenizer
+from pathlib import Path
+
+
+model_id="sentence-transformers/all-MiniLM-L6-v2"
+onnx_path = Path("onnx")
+
+# load vanilla transformers and convert to onnx
+model = ORTModelForFeatureExtraction.from_pretrained(model_id, from_transformers=True)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+# save onnx checkpoint and tokenizer
+model.save_pretrained(onnx_path)
+tokenizer.save_pretrained(onnx_path)
